7037edbad482c4439f09319f47856cd0c7962970
Legan <angelmtzg.09@gmail.com>
2016-10-03 22:31:07 -0500

    Adding the class for Binary Space Partition Tree

--- C_C++/Structures/main.cpp
+++ C_C++/Structures/main.cpp
@@ -4,6 +4,86 @@
 #include <random>
 #include <chrono>
 
+template<size_t d = 1, typename type=float>
+class Vector {
+public:
+protected:
+	type data[d];
+};
+
+
+template<size_t dim=1, typename _Tp = float[dim]>
+class BSP {
+public:
+	class Node {
+	friend class BSP;
+	public:
+
+		Node()
+			:left(NULL),
+			right(NULL), 
+			parent(NULL) {
+		}
+
+	protected:
+		Node* left;
+		Node* right;
+		Node* parent;
+
+		float boundaries[dim][2]; // min and max limits
+
+		float key;
+		std::vector<_Tp> objs;
+	};
+
+	BSP() : root(NULL) {
+	}
+
+	BSP(const std::vector<_Tp>& objs)
+	: root(new Node) {
+		computeSpaceBoundaries(objs);
+	}
+
+protected:
+
+	void computeSpaceBoundaries(const std::vector<_Tp>& objs) {
+		for (size_t i = 0; i < dim; i++) {
+			root->boundaries[i][0] = std::numeric_limits<float>::max(); // min
+			root->boundaries[i][1] = -std::numeric_limits<float>::max(); // max
+		}
+
+		for (size_t i = 0; i < objs.size(); i++) {
+			for (size_t j = 0; j < dim; j++) {
+				if (root->boundaries[j][0] < objs[i][j]) root->boundaries[j][0];
+				if (root->boundaries[j][1] > objs[i][j]) root->boundaries[j][1];
+			}
+		}
+	}
+
+	void splitNode(Node* node, const std::vector<_Tp>& objs) {
+		if (objs.size() == 0) return;
+
+		float dst = 0.0f;
+		std::vector<float> plane = computePlane(node);
+
+		for (size_t i = 0; i < objs.size(); i++) {
+			dst = distanceToPlane();
+			if (dst > 0) {
+			} else {
+			}
+		}
+
+	}
+
+	std::vector<float> computePlane(Node* node) {
+	}
+
+
+
+	Node* root;
+};
+
+
 
 int main(int argc, char** argv) {
 	std::default_random_engine generator
