c7b15f067353fdc4d554b971cdf64bb97b0097bb
Legan <angelmtzg.09@gmail.com>
2016-10-04 18:23:24 -0500

    Adding features for sppliting planes

--- C_C++/Structures/main.cpp
+++ C_C++/Structures/main.cpp
@@ -7,12 +7,46 @@
 template<size_t d = 1, typename type=float>
 class Vector {
 public:
+
+	Vector() {
+		for (size_t i = 0; i < d; i++)
+			data[i] = 0;
+	}
+
+	Vector(const Vector& v) {
+		for (size_t i = 0; i < d; i++)
+			data[i] = v.data[i];
+	}
+
+	Vector<d, type>& operator=(const Vector<d, type>& v) {
+		for (size_t i = 0; i < d; i++)
+			data[i] = v.data[i];
+
+		return *this;
+	}
+
+	type& operator[](size_t i) {
+		return data[i];
+	}
+
+	const type& operator[](size_t i) const {
+		return data[i];
+	}
+
 protected:
 	type data[d];
 };
 
+template<size_t dim=2, size_t p=dim+1>
+float distanceToPlane(const Vector<dim>& point, const Vector<p>& plane) {
+	float eval = 0.0f;
 
-template<size_t dim=1, typename _Tp = float[dim]>
+	for (size_t i = 0; i < dim; i++) eval += plane[i] * point[i];
+
+	return eval + plane[dim];
+}
+
+template<size_t dim=1, typename _Tp = Vector<dim> >
 class BSP {
 public:
 	class Node {
@@ -42,6 +76,13 @@ public:
 	BSP(const std::vector<_Tp>& objs)
 	: root(new Node) {
 		computeSpaceBoundaries(objs);
+
+		for (size_t i = 0; i < dim; i++)
+			printf("dim %lu -> min: %f max: %f", i, root->boundaries[i][0], root->boundaries[i][1]);
+		
+		printf("\n");
+
+		splitNode(root, objs);
 	}
 
 protected:
@@ -54,31 +95,57 @@ protected:
 
 		for (size_t i = 0; i < objs.size(); i++) {
 			for (size_t j = 0; j < dim; j++) {
-				if (root->boundaries[j][0] < objs[i][j]) root->boundaries[j][0];
-				if (root->boundaries[j][1] > objs[i][j]) root->boundaries[j][1];
+				if (objs[i][j] < root->boundaries[j][0]) root->boundaries[j][0] = objs[i][j];
+				if (objs[i][j] > root->boundaries[j][1]) root->boundaries[j][1] = objs[i][j];
 			}
 		}
 	}
 
 	void splitNode(Node* node, const std::vector<_Tp>& objs) {
 		if (objs.size() == 0) return;
+		if (objs.size() == 1) { 
+			node->objs.push_back(objs[0]);
+			return; 
+		}
 
 		float dst = 0.0f;
-		std::vector<float> plane = computePlane(node);
+		std::vector<_Tp> leftObjs, rightObjs;
+
+		Vector<dim+1> plane = computeSplitingPlane(node);
 
 		for (size_t i = 0; i < objs.size(); i++) {
-			dst = distanceToPlane();
-			if (dst > 0) {
-			} else {
+			dst = distanceToPlane<>(objs[i], plane);
+
+			if (dst > 0) leftObjs.push_back(objs[i]);
+			else rightObjs.push_back(objs[i]);
+		}
+
+		printf("Size of left %lu  size of right %lu\n", leftObjs.size(), rightObjs.size());
+
+		node->left = new Node;
+		node->right = new Node;
+		//splitNode(node->left, leftObjs);
+		/*splitNode(node->right, rightObjs);*/
+	}
+
+	Vector<dim+1> computeSplitingPlane(Node* node) {
+		float prevLenght = -std::numeric_limits<float>::max(), length = 0.0f;
+		size_t maxDim = 0;
+
+		for (size_t i = 0; i < dim; i++) {
+			length = node->boundaries[i][1] - node->boundaries[i][0];
+			if (length > prevLenght) {
+				prevLenght = length;
+				maxDim = i; 
 			}
 		}
 
+		Vector<dim + 1> plane;
+		plane[maxDim] = 1.0;
+		plane[dim] = -prevLenght / 2.0f;
+
+		return plane;
 	}
-
-	std::vector<float> computePlane(Node* node) {
-	}
-
-
 
 	Node* root;
 };
@@ -86,6 +153,34 @@ protected:
 
 
 int main(int argc, char** argv) {
+	const size_t dim=2;
+
+	std::default_random_engine generator
+	(std::chrono::system_clock::now().time_since_epoch().count());
+
+	std::uniform_real_distribution<float> d(0, 10);
+	std::vector<Vector<dim> > objs;
+	Vector<dim> v;
+
+	for (size_t i = 0; i < 10; i++) {
+		for (size_t j = 0; j < dim; j++) {
+			v[j] = d(generator);
+		}
+		objs.push_back(v);
+	}
+
+	BSP<dim> bsp(objs);
+
+
+	getchar();
+
+	return 0;
+}
+
+
+
+
+void testBST() {
 	std::default_random_engine generator
 	(std::chrono::system_clock::now().time_since_epoch().count());
 
@@ -115,6 +210,4 @@ int main(int argc, char** argv) {
 	bst.erase(tmp);
 	bst.inOrderWalk();
 	getchar();
-
-	return 0;
 }
